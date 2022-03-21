ae6eadb4995731cd2cd51fdf977efbeb7446e837
David Landa <david.landa@protonmail.com>
2022-03-21 19:06:45 +0100

    Add __main__ module

---
+++ src/cro/schedule/__main__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+The command line interface.
+"""
+
+
+def main() -> None:
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument()
+
+    arguments = parser.parse_args()
