3e3315126e4ce1950e32ad0a07a2fd9c124fd4c5
David Landa <david.landa@protonmail.com>
2022-03-28 10:03:06 +0200

    Move from setup.py to setup.cfg

--- tests/test_package.py
+++ tests/test_package.py
@@ -7,4 +7,4 @@ Test the project as a package e.g. check the version, style etc.
 from cro.schedule import __version__
 
 def test_version():
-    assert __version__ == "0.13.0"
+    assert __version__ == "0.14.0"
