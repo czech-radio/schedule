6cf83e79ce357bc647efb3e8adef01e5b039a8e2
David Landa <david.landa@protonmail.com>
2022-03-28 09:51:42 +0200

    Add __version__ variable and tests

--- src/cro/schedule/__init__.py
+++ src/cro/schedule/__init__.py
@@ -5,4 +5,4 @@ from cro.schedule._domain import Schedule, Show, Station
 
 __all__ = tuple(["Client", "Station", "Show", "Schedule"])
 
-__version__ = "0.14.0"
+__version__ = "0.13.0"
--- tests/test_package.py
+++ tests/test_package.py
@@ -7,4 +7,8 @@ Test the project as a package e.g. check the version, style etc.
 from cro.schedule import __version__
 
 def test_version():
+<<<<<<< HEAD
     assert __version__ == "0.14.0"
+=======
+    assert __version__ == "0.13.0"
+>>>>>>> bf53498 (Add __version__ variable and tests)
