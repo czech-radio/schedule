251d10ad40dedb5d3d7d1cb6382ebf951bea814f
David Landa <david.landa@protonmail.com>
2022-03-28 12:31:13 +0200

    [gh-action] Format the code with black

--- src/cro/schedule/_server/__init__.py
+++ src/cro/schedule/_server/__init__.py
@@ -2,6 +2,20 @@
 
 """
 The server (backend) code to manage (fectch, store) the broadcast schedules.
+
+This subpackage may be later moved to standalone repository.
+
+__Features__
+- [ ] Fetch the schedule from the REST service and show the result.
+- [ ] Allow to save the result to the database.
+      Check wneever the schedules are already saved.
+- [ ] Allow to manage stored schedules.
+      Must be specified
 """
 
-# import flask
+import flask
+# @todo Add to dependencies.
+
+
+def main():
+    app = flask.current_app
