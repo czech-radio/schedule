db8afe782dd1648fcfd9dd05874311f1b4c8f229
David Landa <david.landa@protonmail.com>
2022-03-28 10:04:11 +0200

    Feature/retry mechanism (#2)
    * Add __version__ variable and tests
    
    * Move from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,23 +1,7 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import find_namespace_packages, setup
+import setuptools
 
-NAMESPACE = "cro"
-
-setup(
-    name="cro.schedule",
-    version="0.13.0",
-    package_dir={"": "src"},
-    packages=find_namespace_packages(where="src", include=[f"{NAMESPACE}.*"]),
-    install_requires=[
-        "pandas",
-        "requests",
-        "openpyxl",
-        "tqdm"
-    ],
-    extras_require={
-        "test": ["pytest", "pytest-html"],
-        "docs": ["sphinx", "jupyterlab"],
-        "lint": ["black[jupyter]", "flake8", "isort", "mypy"],
-    },
-)
+if __name__ == "__main__":
+    setuptools.setup()
--- src/cro/schedule/__init__.py
+++ src/cro/schedule/__init__.py
@@ -4,3 +4,5 @@ from cro.schedule._client import Client
 from cro.schedule._domain import Schedule, Show, Station
 
 __all__ = tuple(["Client", "Station", "Show", "Schedule"])
+
+__version__ = "0.14.0"
