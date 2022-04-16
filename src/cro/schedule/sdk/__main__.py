cb9e83ede55381c7dae50ab29b4bc34fcbe432aa
David Landa <david.landa@protonmail.com>
2022-04-16 11:10:55 +0200

    Implement simple CLI (#8)
    * Delete repository test module
    
    * Implement simple CLI

--- src/cro/schedule/sdk/__main__.py
+++ src/cro/schedule/sdk/__main__.py
@@ -1,23 +1,69 @@
 # -*- coding: utf-8 -*-
 
 """
-The command line interface to download the schedules..
+The command line interface to download the schedules.
 """
 
 import argparse
+
+import pandas as pd
+
+from cro.schedule.sdk import Client
 
 __all__ = tuple(["main"])
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser("cro-schedule-cli")
+    parser = argparse.ArgumentParser("The `cro.schedule.sdk` command line interface.")
 
-    parser.add_argument("--station")
-    parser.add_argument("--since")
-    parser.add_argument("--till")
-    parser.add_argument("--output")
-    parser.add_argument("--format")
+    parser.add_argument("-d", "--date", required=True, help="The date of period.")
+    parser.add_argument(
+        "-p",
+        "--period",
+        required=True,
+        help="The period for the schedule e.g D, W, M, Y.",
+    )
+    parser.add_argument(
+        "-s", "--stations", required=True, help="The station for the schedule."
+    )
+    parser.add_argument("-o", "--output", required=False)
+    # parser.add_argument("--format")
 
-    # To be continued.
+    options = parser.parse_args()
 
-    print("--OK--")
+    date: str = options.date
+    period: str = options.period.upper()
+    stations: list[str] = [s.strip() for s in options.stations.split(",")]
+    output = options.output
+
+    client = Client()
+
+    schedule_dfs: list[pd.DataFrame] = []
+
+    def flatten(t):
+        return [item for sublist in t for item in sublist]
+
+    for station in stations:
+        client.station = station.lower()
+
+        match period:
+            case "D":
+                schedules = tuple([client.get_day_schedule(date=date)])
+            case "W":
+                schedules = client.get_week_schedule(date=date)
+            case "M":
+                schedules = client.get_month_schedule(date=date)
+            case _:
+                print(f"Unknown period {period}!")
+
+        schedule_dfs.append([schedule.to_table() for schedule in schedules])
+
+    print(
+        f"Fetched {len(schedules)} schedules for stations {[station.title() for station in stations]} and dates {[schedule.date.isoformat() for schedule in schedules]}."
+    )
+    # Store the result on disk.
+    output_file_path = f"{output}/Schedule_{period}{date}.xlsx"
+    with pd.ExcelWriter(output_file_path) as writer:
+        pd.concat(flatten(schedule_dfs)).to_excel(writer)
+
+    print(f"Result saved to {output_file_path}")
